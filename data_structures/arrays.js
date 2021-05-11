6835a43db62588a4c300c8166b1502bc146df8eb
Gustaw Wypych <gustaw.wypych@gmail.com>
2021-05-11 11:57:48 +0200

    Implementing own Array data structure

--- data_structures/arrays.js
+++ data_structures/arrays.js
@@ -17,7 +17,7 @@ rl.on('line', function (line) {
     }
 
     printArray('insert', array);
-    array.splice(1, 2, 100,111, 112);
+    array.splice(1, 2, 100, 111, 112);
     printArray('splice', array);
 
     printArray('push', array);
@@ -33,3 +33,56 @@ rl.on('line', function (line) {
 });
 
 printArray = (executedAction, array) => console.log(executedAction + ': ', array);
+
+
+class MyArray {
+    constructor() {
+        this.data = {};
+        this.length = 0;
+    }
+
+    push(item) {
+        this.data[this.length++] = item;
+    }
+
+    pop(){
+        const item = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return item;
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        if(item) {
+            this.shiftLeft(index);
+        }
+        return item;
+    }
+
+    shiftLeft(index) {
+        if(index < this.length) {
+            for(let i = index; i < this.length - 1; i++){
+                this.data[i] = this.data[i + 1];
+            }
+            delete this.data[this.length - 1];
+            this.length--;
+        }
+    }
+}
+
+const myArray = new MyArray();
+myArray.push(0);
+myArray.push(1);
+myArray.push(2);
+myArray.push(3);
+myArray.push(4);
+console.log(myArray.get(4));
+console.log(myArray);
+console.log(myArray.delete(4));
+console.log(myArray.pop());
+console.log(myArray);
